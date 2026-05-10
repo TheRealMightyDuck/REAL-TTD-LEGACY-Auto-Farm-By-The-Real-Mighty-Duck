413bb4f1f965a3086c47517b7215d38df1d2d005
TheRealMightyDuck <cacaboyx8@gmail.com>
2026-05-10 03:04:49 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -121,7 +121,7 @@ if (not correctPlace) and (not routeExists) then
 		-0.312460154, 9.48990575e-08, -0.949930847
 	)
 	
-	task.delay(5, function()
+	task.delay(1, function()
 		local args = {
 			{
 				{
