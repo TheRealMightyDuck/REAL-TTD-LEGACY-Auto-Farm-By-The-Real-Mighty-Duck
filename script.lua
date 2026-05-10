7980cf5c9a3b3fe4a649f5f6b90a30cc324f7950
TheRealMightyDuck <cacaboyx8@gmail.com>
2026-05-10 01:52:20 -0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -32,7 +32,7 @@ local function startSkipLoop()
 	task.spawn(function()
 		while skipRunning do
 			fireSkip()
-			task.wait(1)
+			task.wait(0.1)
 		end
 	end)
 end
@@ -92,7 +92,7 @@ else
 	local Queue = {
 		{delay = 15, name = "SpeakerHelicopter",
 			position = vector.create(-75.74588012695312, 2.20206356048584, 97.06993865966797),
-			1
+			extra = 1
 		},
 
 		{delay = 40, name = "SpeakerHelicopter", action = "Upgrade"},
